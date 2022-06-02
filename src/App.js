55981071a0a6d03780e2bb3c89bc217921dfbdec
Jakub Staniek <jakub.staniek@icloud.com>
2022-06-02 15:16:37 +0200

    Fetching data from API, CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,92 @@
+import { useState } from "react";
+
 const api = {
   key: "863fee40b865ab802e428110f41ab09e",
-  base: "https://api.openweathermap.org/data/3.0/",
+  base: "https://api.openweathermap.org/data/2.5/",
+  geo: "http://api.openweathermap.org/geo/1.0/",
 };
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (e) => {
+    e.preventDefault();
+
+    // Get cordinates by name
+    fetch(`${api.geo}direct?q=${query}&limit=1&appid=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        // console.log(result[0].lat);
+
+        // Fetching location
+        fetch(
+          `${api.base}weather?lat=${result[0].lat}&lon=${result[0].lon}&appid=${api.key}`
+        )
+          .then((res) => res.json())
+          .then((result) => {
+            setWeather(result);
+            setQuery("");
+            console.log(result);
+          });
+      });
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
     <div className="container">
       <main>
         <div className="search-box">
-          <input type="text" className="search-bar" placeholder="Search..." />
+          <form onSubmit={search}>
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Search..."
+              onChange={(e) => setQuery(e.target.value)}
+              value={query}
+            />
+          </form>
+        </div>
+        <div className="location-box">
+          <div className="location">{weather.name}</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">{}°C</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
